79f7c445d1bda43bfc371d2c71cd07d6b9556afc
Faezeh <faezehmhmdi78@gmail.com>
2023-10-04 10:41:51 +0000

    refactor: reducing cyclomatic complexity of get_platforms

--- homeassistant/components/xiaomi_miio/__init__.py
+++ homeassistant/components/xiaomi_miio/__init__.py
@@ -136,6 +136,29 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
 
 
+def get_platforms_for_device(model):
+    """Return platforms for a given device model."""
+    model_platforms = {
+        **{model: SWITCH_PLATFORMS for model in MODELS_SWITCH},
+        **{model: HUMIDIFIER_PLATFORMS for model in MODELS_HUMIDIFIER},
+        **{model: FAN_PLATFORMS for model in MODELS_FAN},
+        **{model: LIGHT_PLATFORMS for model in MODELS_LIGHT},
+    }
+
+    if model in model_platforms:
+        return model_platforms[model]
+
+    for vacuum_model in MODELS_VACUUM:
+        if model.startswith(vacuum_model):
+            return VACUUM_PLATFORMS
+
+    for air_monitor_model in MODELS_AIR_MONITOR:
+        if model.startswith(air_monitor_model):
+            return AIR_MONITOR_PLATFORMS
+
+    return []
+
+
 @callback
 def get_platforms(config_entry):
     """Return the platforms belonging to a config_entry."""
@@ -145,20 +168,10 @@ def get_platforms(config_entry):
     if flow_type == CONF_GATEWAY:
         return GATEWAY_PLATFORMS
     if flow_type == CONF_DEVICE:
-        if model in MODELS_SWITCH:
-            return SWITCH_PLATFORMS
-        if model in MODELS_HUMIDIFIER:
-            return HUMIDIFIER_PLATFORMS
-        if model in MODELS_FAN:
-            return FAN_PLATFORMS
-        if model in MODELS_LIGHT:
-            return LIGHT_PLATFORMS
-        for vacuum_model in MODELS_VACUUM:
-            if model.startswith(vacuum_model):
-                return VACUUM_PLATFORMS
-        for air_monitor_model in MODELS_AIR_MONITOR:
-            if model.startswith(air_monitor_model):
-                return AIR_MONITOR_PLATFORMS
+        platforms = get_platforms_for_device(model)
+        if platforms:
+            return platforms
+
     _LOGGER.error(
         (
             "Unsupported device found! Please create an issue at "

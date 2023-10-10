75c82812c52e710cc6e640ff8c6b4b67a2888116
fn890 <fn_890@icloud.com>
2023-10-10 09:11:33 +0000

    M2B:T29: Reduced complexity

--- homeassistant/components/xiaomi_miio/config_flow.py
+++ homeassistant/components/xiaomi_miio/config_flow.py
@@ -217,78 +217,78 @@ class XiaomiMiioFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Configure a xiaomi miio device through the Miio Cloud."""
-        errors = {}
-        if user_input is not None:
-            if user_input[CONF_MANUAL]:
-                return await self.async_step_manual()
+        errors: dict[Any, Any] = {}
+        if user_input is None:
+            return self.async_show_form(
+                step_id="cloud", data_schema=DEVICE_CLOUD_CONFIG, errors=errors
+            )
 
-            cloud_username = user_input.get(CONF_CLOUD_USERNAME)
-            cloud_password = user_input.get(CONF_CLOUD_PASSWORD)
-            cloud_country = user_input.get(CONF_CLOUD_COUNTRY)
+        if user_input[CONF_MANUAL]:
+            return await self.async_step_manual()
 
-            if not cloud_username or not cloud_password or not cloud_country:
-                errors["base"] = "cloud_credentials_incomplete"
-                return self.async_show_form(
-                    step_id="cloud", data_schema=DEVICE_CLOUD_CONFIG, errors=errors
-                )
+        cloud_username = user_input.get(CONF_CLOUD_USERNAME)
+        cloud_password = user_input.get(CONF_CLOUD_PASSWORD)
+        cloud_country = user_input.get(CONF_CLOUD_COUNTRY)
 
-            miio_cloud = MiCloud(cloud_username, cloud_password)
-            try:
-                if not await self.hass.async_add_executor_job(miio_cloud.login):
-                    errors["base"] = "cloud_login_error"
-            except MiCloudAccessDenied:
+        if not cloud_username or not cloud_password or not cloud_country:
+            errors["base"] = "cloud_credentials_incomplete"
+            return self.async_show_form(
+                step_id="cloud", data_schema=DEVICE_CLOUD_CONFIG, errors=errors
+            )
+
+        miio_cloud = MiCloud(cloud_username, cloud_password)
+        try:
+            if not await self.hass.async_add_executor_job(miio_cloud.login):
                 errors["base"] = "cloud_login_error"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception in Miio cloud login")
-                return self.async_abort(reason="unknown")
+        except MiCloudAccessDenied:
+            errors["base"] = "cloud_login_error"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception in Miio cloud login")
+            return self.async_abort(reason="unknown")
 
-            if errors:
-                return self.async_show_form(
-                    step_id="cloud", data_schema=DEVICE_CLOUD_CONFIG, errors=errors
-                )
+        if errors:
+            return self.async_show_form(
+                step_id="cloud", data_schema=DEVICE_CLOUD_CONFIG, errors=errors
+            )
 
-            try:
-                devices_raw = await self.hass.async_add_executor_job(
-                    miio_cloud.get_devices, cloud_country
-                )
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception in Miio cloud get devices")
-                return self.async_abort(reason="unknown")
+        try:
+            devices_raw = await self.hass.async_add_executor_job(
+                miio_cloud.get_devices, cloud_country
+            )
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception in Miio cloud get devices")
+            return self.async_abort(reason="unknown")
 
-            if not devices_raw:
-                errors["base"] = "cloud_no_devices"
-                return self.async_show_form(
-                    step_id="cloud", data_schema=DEVICE_CLOUD_CONFIG, errors=errors
-                )
+        if not devices_raw:
+            errors["base"] = "cloud_no_devices"
+            return self.async_show_form(
+                step_id="cloud", data_schema=DEVICE_CLOUD_CONFIG, errors=errors
+            )
 
-            self.cloud_devices = {}
-            for device in devices_raw:
-                if not device.get("parent_id"):
-                    name = device["name"]
-                    model = device["model"]
-                    list_name = f"{name} - {model}"
-                    self.cloud_devices[list_name] = device
+        self.cloud_devices = {}
+        for device in devices_raw:
+            if not device.get("parent_id"):
+                name = device["name"]
+                model = device["model"]
+                list_name = f"{name} - {model}"
+                self.cloud_devices[list_name] = device
 
-            self.cloud_username = cloud_username
-            self.cloud_password = cloud_password
-            self.cloud_country = cloud_country
+        self.cloud_username = cloud_username
+        self.cloud_password = cloud_password
+        self.cloud_country = cloud_country
 
-            if self.host is not None:
-                for device in self.cloud_devices.values():
-                    cloud_host = device.get("localip")
-                    if cloud_host == self.host:
-                        self.extract_cloud_info(device)
-                        return await self.async_step_connect()
+        if self.host is not None:
+            for device in self.cloud_devices.values():
+                cloud_host = device.get("localip")
+                if cloud_host == self.host:
+                    self.extract_cloud_info(device)
+                    return await self.async_step_connect()
 
-            if len(self.cloud_devices) == 1:
-                self.extract_cloud_info(list(self.cloud_devices.values())[0])
-                return await self.async_step_connect()
+        if len(self.cloud_devices) == 1:
+            self.extract_cloud_info(list(self.cloud_devices.values())[0])
+            return await self.async_step_connect()
 
-            return await self.async_step_select()
-
-        return self.async_show_form(
-            step_id="cloud", data_schema=DEVICE_CLOUD_CONFIG, errors=errors
-        )
+        return await self.async_step_select()
 
     async def async_step_select(
         self, user_input: dict[str, Any] | None = None
